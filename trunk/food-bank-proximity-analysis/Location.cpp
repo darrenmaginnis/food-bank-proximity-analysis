9b5df7c4a2481d925e026f63f3f8aa16e3a4a4c1
overmind.900@gmail.com <overmind.900@gmail.com@ef449a91-615e-61b1-774b-f9834033a573>
2012-11-26 20:22:48 +0000

    Added Location Class

---
+++ trunk/food-bank-proximity-analysis/Location.cpp
@@ -0,0 +1,14 @@
+#include "Location.h"
+
+
+Location::Location(void)
+{
+}
+
+Location::Location(double easting,double northing):Easting(easting), Northing(northing)
+{
+}
+
+Location::~Location(void)
+{
+}

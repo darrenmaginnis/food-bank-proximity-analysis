b0bc082b9d83e89bca8454e4d284b8d1b04a296e
overmind.900@gmail.com <overmind.900@gmail.com@ef449a91-615e-61b1-774b-f9834033a573>
2012-11-26 18:37:12 +0000

    Main skeleton

---
+++ food-bank-proximity-analysis/main.cpp
@@ -0,0 +1,73 @@
+/*
+ * Program: CPA2-6
+ * Course: INFO-5083
+ * Professor: Tony Haworth
+ * Project: Food Bank Proximity Analysis
+ * Due Date: Wed., November 28th, 2012 @ 7:00 p.m.
+ * 
+ * Author Darren Maginnis 0026561
+ * Author Ryan Johnston 0068199
+ * 
+ * Description: an application using MPI to generate statistics about the proximity of residential 
+ *				addresses in the City of Toronto to the available Food Banks.
+ */
+
+
+#include<iostream>
+#include <mpi.h>
+
+
+using namespace std;
+
+int poolSize, instance;
+void processSlave();
+void processMaster();
+
+
+// Function name   : main
+// Description     : Main Application entry
+// Return type     : int
+// Argument        : int argc
+// Argument        : char *argv[]
+
+int main(int argc, char *argv[])
+{
+	//Initialize MPI
+	if( MPI_Init(&argc, &argv) == MPI_SUCCESS )
+	{
+		//Obtain the instance and the poolsize
+		MPI_Comm_size(MPI_COMM_WORLD, &poolSize);
+		MPI_Comm_rank(MPI_COMM_WORLD, &instance);
+		// start 
+		if(instance == 0) 
+			processMaster();
+		else
+			processSlave();
+		
+		//shutdown
+		return MPI_Finalize();
+	}
+	return EXIT_FAILURE;
+}
+
+
+
+// Function name   : processSlave
+// Description     : 
+// Return type     : void
+
+void processSlave(){
+
+
+}
+
+
+
+// Function name   : processMaster
+// Description     : 
+// Return type     :  void
+
+void processMaster(){	
+
+
+}

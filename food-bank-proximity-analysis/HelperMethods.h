cc219abc9421f37aadc37d17a05eb703e0cbfb83
r.johnston2281@gmail.com <r.johnston2281@gmail.com@ef449a91-615e-61b1-774b-f9834033a573>
2012-11-27 19:59:55 +0000

    getNextRes() method created

--- food-bank-proximity-analysis/HelperMethods.h
+++ food-bank-proximity-analysis/HelperMethods.h
@@ -19,12 +19,30 @@ vector<Location> readFile(string fName)
 		while(in>>east>>north)
 		{
 			loc.push_back(Location(east,north));
-
 		}
 
 		return loc;
 }
 
+bool getNextRes(ifstream &f, int offset, Location &loc)
+{
+	
+	double easting, northing;
+	try
+	{
+			for(int i = 0; i < offset; i++)
+			{
+				f >> easting >> northing;		
+			}
+			loc = Location(easting,northing);
+	}
+	catch(...)
+	{
+		return false;
+	}
+
+	return true;
+}
 
 // Function name   : CalcDistace
 // Description     : Calculation of Distance between Locations
@@ -32,7 +50,12 @@ vector<Location> readFile(string fName)
 // Argument        : Location a
 // Argument        : Location b
 
-inline double CalcDistace(Location a, Location b){
+inline double CalcDistance(Location a, Location b){
 	return abs( sqrt( ( pow( ( b.Easting - a.Easting ), 2 ) ) + ( pow( ( b.Northing - a.Northing ), 2 ) ) ) );
 } 
+
+inline double CalcSquareDistance(Location a, Location b){
+	return pow( ( b.Easting - a.Easting ), 2 ) + pow( ( b.Northing - a.Northing ), 2 );
+} 
+
 #endif //__HELPERMETHODS_H__
